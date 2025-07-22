f7f6eab5523376640e5d2d41b912bad6dc2cd999
ubada <ubada_21@hotmail.com>
2025-07-22 01:53:55 -0700

    config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  //API_URL: "https://alansaar-epbcgkf4dbgsahhn.canadacentral-01.azurewebsites.net/api"
-  API_URL: "http://localhost:3000/api"
+  API_URL: "https://alansaar-epbcgkf4dbgsahhn.canadacentral-01.azurewebsites.net/api"
+  //API_URL: "http://localhost:3000/api"
 };
 
 export default config;
