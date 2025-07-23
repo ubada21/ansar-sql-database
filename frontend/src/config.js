c88dd87a16aac9d2bfe5a3fe967864155ff8533f
ubada <ubada_21@hotmail.com>
2025-07-22 23:16:10 -0700

    fixed api_url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: "https://alansaar-epbcgkf4dbgsahhn.canadacentral-01.azurewebsites.net/api"
+  API_URL: "https://alansaar-portal-ajgjdwewgec9c9gy.canadacentral-01.azurewebsites.net/"
   //API_URL: "http://localhost:3000/api"
 };
 
