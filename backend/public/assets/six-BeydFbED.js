185ba4c90f968f0b71e72fc65049a9572e05a564
ubada <ubada_21@hotmail.com>
2025-08-06 19:33:49 -0700

    a

---
+++ backend/public/assets/six-BeydFbED.js
@@ -0,0 +1,1 @@
+import{j as t,e}from"./index-BbQvotS-.js";import{B as a}from"./view-9-Ogogep.js";const s={title:`Page six | Dashboard - ${e.appName}`};function n(){return t.jsxs(t.Fragment,{children:[t.jsx("title",{children:s.title}),t.jsx(a,{title:"Page six"})]})}export{n as default};
